5f58afcfdf3b36d35bd08703c48919365367a416
Cepdadang <acepdadang2203.20@gmail.com>
2022-12-20 22:56:08 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-85236f24a5bc5882.js
+++ _next/static/chunks/webpack-85236f24a5bc5882.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "60338c85d5a0b7b7" + ".css";
+/******/ 			return "static/css/" + "528dd8847cfa232c" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
